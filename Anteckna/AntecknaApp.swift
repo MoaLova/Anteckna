da8825b1c7cfb96f7914266ff73650512a142a3d
”Moa <svensson.moaa@gmail.com>
2024-03-05 23:02:17 +0100

    Initial Commit

---
+++ Anteckna/AntecknaApp.swift
@@ -0,0 +1,20 @@
+//
+//  AntecknaApp.swift
+//  Anteckna
+//
+//  Created by Moa.Svensson on 2024-03-05.
+//
+
+import SwiftUI
+
+@main
+struct AntecknaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
